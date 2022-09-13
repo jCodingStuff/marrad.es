e97c979bff0276c095d6cfc448ec94a127948843
Julián Marrades <julian.marrades@hotmail.es>
2022-09-13 16:34:13 +0200

    Made AppComponent a LanguageComponent and added YT link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,23 @@
 import { Component } from '@angular/core';
-import { LanguageService } from './language.service';
 
 import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faYoutube } from '@fortawesome/free-brands-svg-icons';
+
+import { LanguageComponent } from './language/language.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent extends LanguageComponent {
   
   // Icons
   faPhone = faPhone;
   faEnvelope = faEnvelope;
   faLinkedin = faLinkedin;
   faGithub = faGithub;
-
-  constructor (public langService: LanguageService) {}
-
-  /**
-   * Set a new language for the website
-   * @param language the language to set
-   */
-  setLanguage(language: string): void {
-    this.langService.language = language;
-  }
+  faYoutube = faYoutube;
 
 }
